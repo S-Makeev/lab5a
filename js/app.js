082a4cf4c884875fa4ca64a5157f05f9d631e4f6
Slava Makeev <unclearnuclear@yahoo.com>
2023-02-11 23:32:22 +0000

    added solution to question three

--- js/app.js
+++ js/app.js
@@ -8,12 +8,12 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-// function sum(a, b) { //eslint-disable-line
-//   let mySum = a + b;
-//   let myString = `The sum of ${a} and ${b} is ${mySum}.`;
+function sum(a, b) { //eslint-disable-line
+  let mySum = a + b;
+  let myString = `The sum of ${a} and ${b} is ${mySum}.`;
 
-//   return [mySum, myString];
-// }
+  return [mySum, myString];
+}
 
 // //Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
@@ -38,7 +38,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -53,13 +53,29 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
-// Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+// // Write your code here
 
+function sumAndMultiply(a, b, c) //eslint-disable-line
+{
+
+  let firstAdd =  sum(a, b);
+
+  let AddResult = sum(firstAdd[0], c);
+
+  let addString = `${a} and ${b} and ${c} sum to ${AddResult[0]}.`;
+
+  let firstMult = multiply(a, b);
+  let multResult = multiply(firstMult[0], c);
+
+  let multString = `The product of ${a} and ${b} and ${c} is ${multResult[0]}.`;
+
+
+  console.log(AddResult, multResult, addString, multString);
+  return [AddResult[0], multResult[0], addString, multString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
